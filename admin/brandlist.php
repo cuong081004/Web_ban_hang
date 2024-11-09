f4375d07bc8ee0aade11133ea31131d4bc972560
cuong081004 <2251120336@ut.edu.vn>
2024-11-09 09:41:04 +0700

    back_end

--- admin/brandlist.php
+++ admin/brandlist.php
@@ -11,13 +11,13 @@ $show_brand = $brand->show_brand();
 
 <div class="admin-content-right">
     <div class="admin-content-right-category_list">
-        <h1>Danh sách danh mục</h1>
+        <h1>Danh sách loại sản phẩm</h1>
         <table>
             <tr>
                 <th>STT</th>
                 <th>Id</th>
-                <th>category_id</th>
                 <th>Danh mục</th>
+                <th>Loại sản phẩm</th>
                 <th>Tùy biến</th>
             </tr>
             <?php
@@ -29,7 +29,7 @@ $show_brand = $brand->show_brand();
             <tr>
                 <td><?php echo $i ?></td>
                 <td><?php echo $result['brand_id'] ?></td>
-                <td><?php echo $result['category_id'] ?></td>
+                <td><?php echo $result['category_name'] ?></td>
                 <td><?php echo $result['brand_name'] ?></td>
                 <td>
                     <a href="brandedit.php?brand_id=<?php echo $result['brand_id'] ?>">Sửa</a>
--- admin/class/brand_class.php
+++ admin/class/brand_class.php
@@ -15,7 +15,6 @@ class brand {
     {
         $query ="INSERT INTO tbl_brand (category_id, brand_name) VALUES ('$category_id','$brand_name')";
         $result = $this ->db -> insert($query);
-        header("Lacotien");
         header('location:brandlist.php');
         return $result;
     }
@@ -29,28 +28,28 @@ class brand {
     public function show_brand()
     {
         // $query ="SELECT * FROM tbl_brand ORDER BY brand_id DESC";
-        $query ="SELECT tbl_brand*,tbl_category.category_name
-                 FROM tbl_brand INNER JOIN tbl_category ON tbl_brand.category_id = tbl.category.category_id
+        $query ="SELECT tbl_brand.*, tbl_category.category_name
+                 FROM tbl_brand INNER JOIN tbl_category ON tbl_brand.category_id = tbl_category.category_id
                  ORDER BY tbl_brand.brand_id DESC";
         $result = $this ->db -> select($query);
         return $result;
     }
 
-    public function get_category($category_id,){
-        $query ="SELECT *FROM tbl_category WHERE category_id= '$category_id'";
+    public function get_brand($brand_id,){
+        $query ="SELECT * FROM tbl_brand WHERE brand_id = '$brand_id'";
         $result = $this ->db -> select($query);
         return $result;
     }
-    public function update_category($category_id, $category_name){
-        $query = "UPDATE tbl_category SET category_name = '$category_name' WHERE category_id= '$category_id' ";
+    public function update_brand($category_id, $brand_name, $brand_id){
+        $query = "UPDATE tbl_brand SET brand_name = '$brand_name', category_id='$category_id' WHERE brand_id= '$brand_id' ";
         $result = $this ->db -> update($query);
-        header('location:categorylist.php');
+        header('location:brandlist.php');
         return $result;
     }
-    public function delete_category($category_id){
-        $query = "DELETE FROM tbl_category WHERE category_id= '$category_id'";
+    public function delete_brand($brand_id){
+        $query = "DELETE FROM tbl_brand WHERE brand_id= '$brand_id'";
         $result = $this ->db -> delete($query);
-        header('location:categorylist.php');
+        header('location:brandlist.php');
         return $result;
     }
 }
